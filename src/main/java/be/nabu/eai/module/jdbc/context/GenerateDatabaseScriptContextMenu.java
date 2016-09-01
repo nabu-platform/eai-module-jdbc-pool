7678a423608fce01f5eacfe353fb39097486dfb0
alex <alex@nabu.be>
2016-09-01 16:07:34 +0200

    Added support for insert creation

--- src/main/java/be/nabu/eai/module/jdbc/context/GenerateDatabaseScriptContextMenu.java
+++ src/main/java/be/nabu/eai/module/jdbc/context/GenerateDatabaseScriptContextMenu.java
@@ -1,26 +1,40 @@
 package be.nabu.eai.module.jdbc.context;
 
+import java.util.LinkedHashSet;
+import java.util.List;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
+import be.nabu.eai.developer.MainController;
 import be.nabu.eai.developer.api.EntryContextMenuProvider;
+import be.nabu.eai.developer.managers.base.BaseConfigurationGUIManager;
+import be.nabu.eai.developer.managers.util.SimplePropertyUpdater;
 import be.nabu.eai.developer.util.Confirm;
+import be.nabu.eai.developer.util.EAIDeveloperUtils;
 import be.nabu.eai.developer.util.Confirm.ConfirmType;
 import be.nabu.eai.repository.EAIRepositoryUtils;
 import be.nabu.eai.repository.api.Entry;
+import be.nabu.libs.property.api.Property;
 import be.nabu.libs.services.jdbc.api.SQLDialect;
+import be.nabu.libs.types.TypeUtils;
+import be.nabu.libs.types.api.ComplexContent;
 import be.nabu.libs.types.api.ComplexType;
+import be.nabu.libs.types.api.Element;
+import be.nabu.libs.types.base.ComplexElementImpl;
 
 public class GenerateDatabaseScriptContextMenu implements EntryContextMenuProvider {
 
 	@Override
 	public MenuItem getContext(Entry entry) {
 		if (entry.isNode() && ComplexType.class.isAssignableFrom(entry.getNode().getArtifactClass())) {
-			Menu menu = new Menu("Create SQL");
+			Menu menu = new Menu("SQL");
+			Menu create = new Menu("Create DDL");
+			Menu insert = new Menu("Insert DML");
 			for (final Class<SQLDialect> clazz : EAIRepositoryUtils.getImplementationsFor(entry.getRepository().getClassLoader(), SQLDialect.class)) {
-				MenuItem item = new MenuItem(clazz.getSimpleName());
-				item.addEventHandler(ActionEvent.ANY, new EventHandler<ActionEvent>() {
+				MenuItem createItem = new MenuItem(clazz.getSimpleName());
+				createItem.addEventHandler(ActionEvent.ANY, new EventHandler<ActionEvent>() {
 					@Override
 					public void handle(ActionEvent arg0) {
 						try {
@@ -31,8 +45,43 @@ public class GenerateDatabaseScriptContextMenu implements EntryContextMenuProvid
 						}
 					}
 				});
-				menu.getItems().add(item);
+				create.getItems().add(createItem);
+				
+				MenuItem insertItem = new MenuItem(clazz.getSimpleName());
+				insertItem.addEventHandler(ActionEvent.ANY, new EventHandler<ActionEvent>() {
+					@SuppressWarnings({ "unchecked", "rawtypes" })
+					@Override
+					public void handle(ActionEvent arg0) {
+						try {
+							List<Property<?>> supported = BaseConfigurationGUIManager.createProperty(new ComplexElementImpl((ComplexType) entry.getNode().getArtifact(), null));
+							SimplePropertyUpdater updater = new SimplePropertyUpdater(true, new LinkedHashSet(supported));
+							EAIDeveloperUtils.buildPopup(MainController.getInstance(), updater, "Insert Values", new EventHandler<ActionEvent>() {
+								@Override
+								public void handle(ActionEvent arg0) {
+									try {
+										ComplexContent newInstance = ((ComplexType) entry.getNode().getArtifact()).newInstance();
+										for (Element<?> element : TypeUtils.getAllChildren(newInstance.getType())) {
+											Object value = updater.getValue(element.getName());
+											if (value != null) {
+												newInstance.set(element.getName(), value);
+											}
+										}
+										Confirm.confirm(ConfirmType.INFORMATION, "Insert SQL: " + entry.getId(), clazz.newInstance().buildInsertSQL(newInstance), null);
+									}
+									catch (Exception e) {
+										e.printStackTrace();
+									}
+								}
+							});
+						}
+						catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				});
+				insert.getItems().add(insertItem);
 			}
+			menu.getItems().addAll(create, insert);
 			return menu;
 		}
 		else if (!entry.isLeaf()) {
--- src/main/java/be/nabu/eai/module/jdbc/dialects/PostgreSQL.java
+++ src/main/java/be/nabu/eai/module/jdbc/dialects/PostgreSQL.java
@@ -1,7 +1,9 @@
 package be.nabu.eai.module.jdbc.dialects;
 
 import java.net.URI;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,6 +15,7 @@ import be.nabu.eai.repository.EAIRepositoryUtils;
 import be.nabu.libs.property.ValueUtils;
 import be.nabu.libs.services.jdbc.api.SQLDialect;
 import be.nabu.libs.types.TypeUtils;
+import be.nabu.libs.types.api.ComplexContent;
 import be.nabu.libs.types.api.ComplexType;
 import be.nabu.libs.types.api.Element;
 import be.nabu.libs.types.api.SimpleType;
@@ -163,5 +166,61 @@ public class PostgreSQL implements SQLDialect {
 		else {
 			return null;
 		}
+	}
+
+	@Override
+	public String buildInsertSQL(ComplexContent content) {
+		StringBuilder keyBuilder = new StringBuilder();
+		StringBuilder valueBuilder = new StringBuilder();
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+		formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+		Date date = new Date();
+		for (Element<?> element : TypeUtils.getAllChildren(content.getType())) {
+			if (element.getType() instanceof SimpleType) {
+				Class<?> instanceClass = ((SimpleType<?>) element.getType()).getInstanceClass();
+				if (!keyBuilder.toString().isEmpty()) {
+					keyBuilder.append(",\n\t");
+					valueBuilder.append(",\n\t");
+				}
+				keyBuilder.append(EAIRepositoryUtils.uncamelify(element.getName()));
+				Object value = content.get(element.getName());
+				Integer minOccurs = ValueUtils.getValue(MinOccursProperty.getInstance(), element.getProperties());
+				// if there is no value but it is mandatory, try to generate one
+				if (value == null && minOccurs != null && minOccurs > 0) {
+					if (UUID.class.isAssignableFrom(instanceClass)) {
+						value = UUID.randomUUID();
+					}
+					else if (Date.class.isAssignableFrom(instanceClass)) {
+						value = date;
+					}
+					else if (Number.class.isAssignableFrom(instanceClass)) {
+						value = 0;
+					}
+					else if (Boolean.class.isAssignableFrom(instanceClass)) {
+						value = false;
+					}
+				}
+				if (value == null) {
+					valueBuilder.append("null");
+				}
+				else {
+					boolean closeQuote = false;
+					if (Date.class.isAssignableFrom(instanceClass)) {
+						valueBuilder.append("timestamp '").append(formatter.format(value)).append("'");
+					}
+					else {
+						if (URI.class.isAssignableFrom(instanceClass) || String.class.isAssignableFrom(instanceClass) || UUID.class.isAssignableFrom(instanceClass)) {
+							valueBuilder.append("'");
+							closeQuote = true;
+						}
+						valueBuilder.append(value.toString());
+						if (closeQuote) {
+							valueBuilder.append("'");
+						}
+					}
+				}
+			}
+		}
+		return "insert into " + EAIRepositoryUtils.uncamelify(content.getType().getName()) + "(\n\t" + keyBuilder.toString() + "\n) values (\n\t" + valueBuilder.toString() + "\n);";
 	}
 }
